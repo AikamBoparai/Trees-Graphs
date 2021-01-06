14e267871ed02fa5b0f221ab305271b606e38011
AIKAM BOPARAI <as2bopar@uwaterloo.ca>
2021-01-06 01:54:06 -0500

    BSTSequence extra code removed

--- BSTSequence.java
+++ BSTSequence.java
@@ -63,10 +63,8 @@ class BSTSequence{
         Node n1 = new Node(7);
         Node n2 = new Node(4);
         Node n3 = new Node(11);
-        Node n4 = new Node(2);
         Node n5 = new Node(5);
         Node n6 = new Node(9);
-        Node n7 = new Node(10);
 
         n1.left = n2;
         n1.right = n3;
